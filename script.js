ddeec6ab9cc82ee5455070f0c0674b5ca1032c79
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-11 22:54:00 +0000

    Styling and add link back to the source website

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = ``;  //clearing textContent
   
+  //Add container and class for styling
+  const episodesContainer = document.createElement("div");
+  episodesContainer.className = "episodes-container"; // 
+
   // for each episode make new card with <div>
   
-  //
    for(const episode of episodeList){
     const episodeCard = document.createElement("div"); 
+    episodeCard.className = "episode-card"; // add class for card for styling
     
     //each card includes title, episodeCode, name, image and summary
     const episodeSeason = String(episode.season).padStart(2, "0");
@@ -21,13 +25,18 @@ function makePageForEpisodes(episodeList) {
 
     episodeCard.innerHTML =`
     <h2>${episode.name} (S${episodeSeason}E${episodeNumber})</h2>
-    <img src="${episode.image.medium}">
+    <img src="${episode.image.medium}" alt="${episode.name}">
     <p>${episode.summary}</p>
-    `;
-  // Append the episode card to the rootElem
-  rootElem.append(episodeCard);
+    `;  
+    // each episode can open in new window for more detail (from the source website)
+    episodeCard.addEventListener("click", () => {
+      window.open(episode.url, "_blank"); 
+    });
+  // Append the episode card to the container
+  episodesContainer.append(episodeCard);
   };
-  
+  // Append the container card to the root
+  rootElem.append(episodesContainer);
 }
 
  window.onload = setup;
