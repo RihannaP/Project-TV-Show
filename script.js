e261166fa8198725b594c5a0f1a702a238305484
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-11 19:01:44 +0000

    test for oneEpisode

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
+ function setup() {
+   const allEpisodes = getOneEpisode();
   makePageForEpisodes(allEpisodes);
-}
+ }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  rootElem.textContent = ``;
 
-window.onload = setup;
+  const episodeCard = document.createElement("div");
+  episodeCard.innerHTML =`
+    <h2>${episodeList.name}</h2>
+    <img src="${episodeList.image.medium}">
+    <p>${episodeList.summary}</p>
+  `
+  rootElem.append(episodeCard);
+ }
+
+ window.onload = setup;
+
+//  function makeFilmCard (parentElement, tagName, textContent ){
+//   const element = document.createElement(tagName);
+//   element.textContent = textContent;
+//   parentElement.append(element);
+//   return element;
+// }
+// const card = document.createElement("section")
+// makeFilmCard(card, "h3", filmTest.title);
+// makeFilmCard(card, "p", filmTest.director);
+// makeFilmCard(card, "time", filmTest.times);
+// makeFilmCard(card, "date", filmTest.certificate);
+// document.body.append(card);
